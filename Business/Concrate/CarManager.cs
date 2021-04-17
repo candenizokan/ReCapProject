9d1c4788d3eef79c5a59f7193a3358ae60c8e6ab
OKAN CANDENİZ <candenizokan@gmail.com>
2021-04-17 22:05:01 +0300

    InMemory

---
+++ Business/Concrate/CarManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using DataAccess.Concrate.InMemory;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrate
+{
+    public class CarManager : ICarService
+    {
+        InMemoryCarDal _inMemoryCarDal;
+
+        public CarManager(InMemoryCarDal inMemoryCarDal)
+        {
+            _inMemoryCarDal = inMemoryCarDal;
+        }
+        public List<Car> GetAll()
+        {
+            return _inMemoryCarDal.GetAll();
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            if (Id<5)
+            {
+                return _inMemoryCarDal.GetById(Id);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrate;
+using DataAccess.Concrate.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager car = new CarManager(new InMemoryCarDal());
+
+            foreach (var item in car.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+            
+        }
+    }
+}
